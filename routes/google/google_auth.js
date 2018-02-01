842fac5cf86ed8fe1dd5bfa7ff86b8133c2de8e0
bedhed <clement.dietschy@gmail.com>
2018-02-01 11:28:57 +0100

    Starting Rebranding.

--- routes/google/google_auth.js
+++ routes/google/google_auth.js
@@ -27,6 +27,7 @@ var UrlHelper = require('../../helpers/url_helper.js');
 // Create Google OAuth2 Client for everyone
 // Populate with tokens if available
 // @todo deduplicate this code (also in admin.js)
+// @todo the oauth2client generated can be set as a global auth option to be used everywhere...
 router.use(function(req, res, next) {
   req.googleOAuth = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
