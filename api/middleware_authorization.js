87ddf47e5198a4410e1ff1dc33bae2d3b4327c05
Quentin Drumez <quentin.drumez@isen.yncrea.fr>
2018-12-03 13:12:44 +0100

    Correct authorization for get and delete request method (#200)
    * Create authorization middleware and refacto Record API
    
    * minor update
    
    * Refacto organisations API
    
    * Add organisation for public field : _id
    
    * minor update
    
    * Error catch
    
    * Add record sanitizer to API
    
    * Correct authorization for get and delete request method

--- api/middleware_authorization.js
+++ api/middleware_authorization.js
@@ -1,20 +1,46 @@
 var express = require('express');
 var router = express.Router();
 var Organisation = require('../models/organisation');
+var Record = require('../models/record');
 
+/**
+ * @description The easy way to get authorization is to provide orgId in the body of the request.
+ *              The other way (for get and delete methods) is that we find orgId thanks to recordId.
+ */
 router.use(function(req, res, next) {
-    if(!req.body.orgId) return res.status(422).json({message: 'Missing parameter'});
-    Organisation.findOne({'_id': req.body.orgId})
-    .then(organisation => {
-        if(!organisation) return res.status(404).json({message: 'Organisation not found'});
-        if( !req.user || (!req.user.isSuperAdmin() && !req.user.belongsToOrganisation(organisation._id)) ) 
-            return res.status(403).json({message: 'You haven\'t access to this Organisation.'});
-
-        req.organisation = organisation;
-        return next();
-    }).catch(err => {
-        return res.status(500).json({message: 'Internal error', errors: [err]});
-    });
+    let recordId = req.originalUrl.split('/api/profiles/')[1].split('/')[0].trim();
+    if(!req.body.orgId && !recordId) return res.status(422).json({message: 'Missing parameter'});
+    if(req.body.orgId){
+        Organisation.findOne({'_id': req.body.orgId})
+        .then(organisation => {
+            if(!organisation) return res.status(404).json({message: 'Organisation not found'});
+            if( !req.user || (!req.user.isSuperAdmin() && !req.user.belongsToOrganisation(organisation._id)) ) 
+                return res.status(403).json({message: 'You haven\'t access to this Organisation.'});
+    
+            req.organisation = organisation;
+            return next();
+        }).catch(err => {
+            return res.status(500).json({message: 'Internal error', errors: [err]});
+        });
+    }else if(recordId){
+        Record.findOne({'_id': recordId})
+        .then(record => {
+            if(!record) return res.status(404).json({message: 'Record not found'});
+            if( !req.user || (!req.user.isSuperAdmin() && !req.user.belongsToOrganisation(record.organisation)) ) 
+                return res.status(403).json({message: 'You haven\'t access to this Organisation.'});
+                Organisation.findOne({'_id': record.organisation})
+                .then(organisation => {
+                    if(!organisation) return res.status(404).json({message: 'Organisation not found'});
+                    req.organisation = organisation;
+                    return next();
+                }).catch(err => {
+                    return res.status(500).json({message: 'Internal error', errors: [err]});
+                });
+        }).catch(err => {
+            return res.status(500).json({message: 'Internal error', errors: [err]});
+        });
+    }
+    
 });
 
 module.exports = router;
