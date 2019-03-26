8668aa3efa7c11808f623009bf7e4977e4f9b307
Quentin Drumez <quentin.drumez@isen.yncrea.fr>
2019-03-26 16:43:44 +0100

    API user : add welcome route (#294)

--- api/user/api_user.js
+++ api/user/api_user.js
@@ -26,6 +26,36 @@ router.get('/current', passport.authenticate('bearer', {session: false}), functi
 });
 
 /**
+ * @api {put} /api/users/:userId/organisation/:orgId Welcome user in organisation
+ * @apiName UpdateUser
+ * @apiGroup User
+ * @apiVersion 0.9.0
+ * 
+ * @apiHeader {String} Authorization User 'Bearer access_token'
+ * 
+ * @apiSuccess {String} message User updated with success.
+ * @apiSuccess {User} user User updated
+ * 
+ * @apiError (500 Internal Server Error) InternalError Internal error
+ * @apiError (400 Bad Request) BadRequest Missing parameters
+ * @apiError (401 Unauthorized) InvalidGrant Invalid resource owner credentials.
+ * @apiError (403 Unauthorized) Unauthorized Client id or secret invalid. OR Your are not allowed to update this User.
+ * @apiError (422 Missing Parameters) MissingParameters Missing parameters.
+ */
+router.put('/welcome/:userId/organisation/:orgId', passport.authenticate('bearer', {session: false}), (req, res, next) => {
+  if( (req.user._id !== req.params.userId) && !req.user.isSuperAdmin())
+    return res.status(403).json({message: 'Your are not allowed to update this User.'});
+
+  User.findOne({_id: req.params.userId})
+  .then((user) => {
+    user.welcomeToOrganisation(req.params.orgId, (err, userUpdated) => {
+      if(err) return res.status(404).json({message: 'User is not linked to this organisation.'});
+      return res.status(200).json({message: 'User welcomed to organisation.', user: userUpdated});
+    });
+  });
+});
+
+/**
  * @api {put} /api/users/:userId? Update current user or other user
  * @apiName UpdateUser
  * @apiGroup User
@@ -42,19 +72,19 @@ router.get('/current', passport.authenticate('bearer', {session: false}), functi
  * @apiError (403 Unauthorized) Unauthorized Client id or secret invalid. OR Your are not allowed to update this User.
  * @apiError (422 Missing Parameters) MissingParameters Missing parameters.
  */
-router.put('/:userId?', passport.authenticate('bearer', {session: false}), function(req, res, next) {
-    if(!req.body.user) return res.status(422).json({message: 'Missing parameter'});
-    if(!req.user.isSuperAdmin() && req.params.userId) return res.status(403).json({message: 'Your are not allowed to update this User.'});
+// router.put('/:userId?', passport.authenticate('bearer', {session: false}), function(req, res, next) {
+//     if(!req.body.user) return res.status(422).json({message: 'Missing parameter'});
+//     if(!req.user.isSuperAdmin() && req.params.userId) return res.status(403).json({message: 'Your are not allowed to update this User.'});
 
-    let userId = req.params.userId ? req.params.userId : req.user._id;
-    if(req.body.user.superadmin && !req.user.isSuperAdmin()) delete req.body.user.superadmin;
+//     let userId = req.params.userId ? req.params.userId : req.user._id;
+//     if(req.body.user.superadmin && !req.user.isSuperAdmin()) delete req.body.user.superadmin;
 
-    User.findOneAndUpdate({'_id' : userId}, {$set: req.body.user}, {new: true})
-    .then(userUpdated => {
-        if(!userUpdated) return res.status(404).json({message: 'User not found.'});
-        return res.status(200).json({message: 'User updated with success.', user: userUpdated});
-    }).catch((err) => {return next(err);});  
-});
+//     User.findOneAndUpdate({'_id' : userId}, {$set: req.body.user}, {new: true})
+//     .then(userUpdated => {
+//         if(!userUpdated) return res.status(404).json({message: 'User not found.'});
+//         return res.status(200).json({message: 'User updated with success.', user: userUpdated});
+//     }).catch((err) => {return next(err);});  
+// });
 
 router.use(function(err, req, res, next){
     if(err) return res.status(500).json({message: 'Internal error', errors: [err.message]});
