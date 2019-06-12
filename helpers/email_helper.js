09af76070fec332a8b31b285b55e1f38e893de71
mezdru <quentin.drumez@isen.yncrea.fr>
2019-06-12 15:13:37 +0200

    Update text

--- helpers/email_helper.js
+++ helpers/email_helper.js
@@ -392,22 +392,22 @@ var EmailHelper = {
           .request({
             "FromEmail": defaultEmitter,
             "FromName": defaultEmitterName,
-            "Subject": res.__("Welcome to {{orgName}} {{firstName}}!", {orgName: organisation.name, firstName: firstName}),
+            "Subject": res.__("Spread your Wings {{firstName}}!", {firstName: firstName}),
             "MJ-TemplateID": "854412",
             "MJ-TemplateLanguage": true,
             "Recipients": [
               { "Email": recipientEmail }
             ],
             "Vars": {
-              "title": res.__("Welcome to {{orgName}} {{firstName}}!", {orgName: organisation.name, firstName: firstName}),
-              "text": res.__("Congratulation!<br/> You are registered to the Wingzy of {{orgName}}.<br/> Begin looking for helps now!",
-                              {orgName: organisation.name}),
-              "ctaText": res.__("Go to {{orgName}}", {orgName: organisation.name}),
+              "title": res.__("Welcome to the Wingzy of {{orgName}}", {orgName: organisation.name}),
+              "text": res.__("{{firstName}},<br/>Thank for sharing your Wings with the people of {{orgName}}. <br/>Now, everytime you need help go to Wingzy to find it (and much more)!",
+                              {orgName: organisation.name, firstName: firstName}),
+              "ctaText": res.__("Search {{orgName}}", {orgName: organisation.name}),
               "squareIcon": "https://ucarecdn.com/5a0ec475-a18d-498e-809c-9c9f2911a658/",
               "orgBannerUrl": (organisation && organisation.cover ? organisation.cover.url || defaultBannerUrl : defaultBannerUrl),
               "orgLogoUrl": (organisation && organisation.logo ? organisation.logo.url || defaultLogoUrl : defaultLogoUrl),
               "ctaUrl": url || defaultLink,
-              "outro": res.__("For any questions, <a href='mailto:contact@wingzy.com'>contact us.</a>")
+              "outro": res.__("Got any question? feedback? advise? <a href='mailto:contact@wingzy.com'>Contact us!</a>")
             }
           });
         return request;
