12f933dce06d7b0ce6435835725297da3df38e37
mezdru <quentin.drumez@isen.yncrea.fr>
2019-06-13 11:50:54 +0200

    Up client auth request

--- helpers/client_auth_helper.js
+++ helpers/client_auth_helper.js
@@ -7,15 +7,19 @@ class ClientAuthHelper {
 	fetchClientAccessToken() {
 		return new Promise((resolve, reject) => {
 			request.post({
-				url: (process.env.NODE_ENV == 'development' ? 'http://' : 'https://') + `${process.env.HOST_AUTH}/accessToken`,
+				url: (process.env.NODE_ENV == 'development' ? 'http://' : 'https://') + `${process.env.HOST_AUTH}/locale`,
 				json: true,
-        body: {client_id: process.env.LOCALE_CLIENT_ID, client_secret: process.env.LOCALE_CLIENT_SECRET}
+        body: {
+					client_id: process.env.LOCALE_CLIENT_ID, 
+					client_secret: process.env.LOCALE_CLIENT_SECRET,
+					grant_type: 'client_credentials'
+				}
 			}, (error, requestResponse, body) => {
 
 				if (error || (body && body.status && body.status !== 200) || (requestResponse.statusCode !== 200)) {
 					return reject(error);
 				}
-				return resolve(body.accessToken);
+				return resolve(body.access_token);
 			});
 		});
 	}
