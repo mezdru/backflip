49587ca48cdd09ff80439673b27e66dd76490139
mezdru <quentin.drumez@isen.yncrea.fr>
2019-06-28 15:48:44 +0200

    up

---
+++ api/routes/organisation.api.js
@@ -0,0 +1,40 @@
+let express = require('express');
+var passport = require('passport');
+
+let OrganisationController = require('../../controllers/organisation.controller');
+var AuthorizationOrganisation = require('../authorization/organisation.authorization');
+var Authorization = require('../authorization/access.authorization');
+require('../passport/strategy');
+
+let router = express.Router();
+
+router.get(
+  '/forPublic',
+  OrganisationController.getSingleOrganisationForPublic,
+  Authorization.resWithData
+);
+
+router.get(
+  '/:id/algolia/public',
+  OrganisationController.getalgoliaPublicKey,
+  Authorization.resWithData
+);
+
+router.get(
+  '/:id/algolia/private',
+  passport.authenticate('bearer', {session: false}),
+  AuthorizationOrganisation,
+  OrganisationController.getAlgoliaPrivateKey,
+  Authorization.resWithData
+);
+
+router.get(
+  '/:id',
+  passport.authenticate('bearer', {session: false}),
+  AuthorizationOrganisation,
+  OrganisationController.getSingleOrganisation,
+  Authorization.resWithData
+);
+
+
+module.exports = router;
--- controllers/organisation.controller.js
+++ controllers/organisation.controller.js
@@ -1,11 +1,12 @@
 var Organisation = require('../models/organisation');
+var algoliaOrganisation = require('../models/algolia/algolia_organisation');
 
 exports.getSingleOrganisationForPublic = (req, res, next) => {
-  Organisation.findOne({tag: req.params.tag})
+  Organisation.findOne({... req.query})
   .then(organisation => {
 
     if(!organisation) {
-      req.backflip = {message: 'Organisation not found by tag', status: 404};
+      req.backflip = {message: 'Organisation not found', status: 404};
     } else {
       req.backflip = {message: 'Organisation found for public', status: 200, data: {
         _id: organisation._id,
@@ -29,6 +30,30 @@ exports.getSingleOrganisation = (req, res, next) => {
       req.backflip = {message: 'Organisation found', status: 200, data: organisation};
     }
     return next();
+  }).catch(err => next(err));
+}
+
+exports.getAlgoliaPrivateKey = (req, res, next) => {
+  Organisation.findOne({_id: req.params.id})
+  .then(organisation => {
+    if(!organisation) {
+      req.backflip = {message: 'Organisation not found.', status: 404};
+    } else {
+      let publicKey = algoliaOrganisation.makePublicKey(organisation._id);
+      req.backflip = {message: 'Organisation algolia key found', status: 200, data: publicKey};
+    }
+  }).catch(err => next(err));
+}
+
+exports.getAlgoliaPublicKey = (req, res, next) => {
+  Organisation.findOne({_id: req.params.id, public: true})
+  .then(organisation => {
+    if(!organisation) {
+      req.backflip = {message: 'Organisation public not found.', status: 404};
+    } else {
+      let publicKey = algoliaOrganisation.makePublicKey(organisation._id);
+      req.backflip = {message: 'Organisation algolia key found', status: 200, data: publicKey};
+    }
   }).catch(err => next(err));
 }
 
