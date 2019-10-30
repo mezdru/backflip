8d36506a189da79190d33dc738fc9baa75235b5e
mezdru <quentin.drumez@isen.yncrea.fr>
2019-10-30 11:24:30 +0100

    Add keen bulk method

--- helpers/keen_helper.js
+++ helpers/keen_helper.js
@@ -11,9 +11,28 @@ const clientKeen = new KeenAnalysis({
  */
 class KeenHelper {
 
+  async recordEventBulk(object) {
+    const clientKeenTracking = new KeenTracking({
+      projectId: process.env.KEEN_PROJECT_ID,
+      writeKey: process.env.KEEN_WRITES_KEY
+    });
+
+
+    clientKeenTracking
+      .recordEvents(object)
+      .then((response) => {
+        console.log(response)
+        // handle successful responses
+      })
+      .catch(error => {
+        console.log(error);
+        // handle errors
+      });
+  }
+
   async recordEvent(eventFamily, object, orgId) {
-    let keenKeyObject = await this.findOrCreateAccessKey(orgId, 'writes', false).catch(e => {console.log(e); return null;});
-    if(!keenKeyObject || !keenKeyObject.key) return null;
+    let keenKeyObject = await this.findOrCreateAccessKey(orgId, 'writes', false).catch(e => { console.log(e); return null; });
+    if (!keenKeyObject || !keenKeyObject.key) return null;
 
     const clientKeenTracking = new KeenTracking({
       projectId: process.env.KEEN_PROJECT_ID,
@@ -22,26 +41,26 @@ class KeenHelper {
 
     return await clientKeenTracking.recordEvent(eventFamily, {
       item: object
-    }).catch(e => {console.log(e); return null;});
+    }).catch(e => { console.log(e); return null; });
   }
 
   async findOrCreateAccessKey(orgId, type, isPublic) {
     let org = (isPublic ?
-      await Organisation.findOne({_id: orgId, public: isPublic}).catch(e => null) :
-      await Organisation.findOne({_id: orgId}).catch(e => null));
+      await Organisation.findOne({ _id: orgId, public: isPublic }).catch(e => null) :
+      await Organisation.findOne({ _id: orgId }).catch(e => null));
 
     if (!org) return null;
-  
+
     org = JSON.parse(JSON.stringify(org));
-  
+
     let keyObjectArray = await clientKeen
       .get({
-        url: clientKeen.url('projectId', `keys?name=${(org._id+':'+type)}`),
+        url: clientKeen.url('projectId', `keys?name=${(org._id + ':' + type)}`),
         api_key: clientKeen.masterKey(),
       }).catch(e => { console.log(e); return null; });
-  
+
     let keyObject = (keyObjectArray.length > 0 ? keyObjectArray[0] : null);
-  
+
     let options = (type === 'queries' ? {
       queries: {
         filters: [
@@ -53,23 +72,23 @@ class KeenHelper {
         ]
       }
     } : {
-      writes: {
-        autofill: {
-          organisation: {
-            _id: org._id,
-            tag: org.tag
+        writes: {
+          autofill: {
+            organisation: {
+              _id: org._id,
+              tag: org.tag
+            }
           }
         }
-      }
-    })
-  
+      })
+
     if (!keyObject) {
       keyObject = await clientKeen
         .post({
           url: clientKeen.url('projectId', 'keys'),
           api_key: clientKeen.masterKey(),
           params: {
-            name: (org._id+':'+type),
+            name: (org._id + ':' + type),
             isActive: true,
             permitted: [type],
             options: options
