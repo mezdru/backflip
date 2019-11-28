09b5f771d16e9b1373f4a1c0becfe5a3bcd214f2
mezdru <quentin.drumez@isen.yncrea.fr>
2019-11-28 15:32:48 +0100

    Fix: agenda workflow

--- models/agenda_scheduler.js
+++ models/agenda_scheduler.js
@@ -24,13 +24,13 @@ var Agenda = (function () {
      */
     this.agenda.define('sendEmailConfirmation', async (job, done) => {
       let user = await User.findOne({ _id: job.attrs.data.userId });
+      await this.removeJob(job);
 
       if (!user.email.validated) {
         await AgendaController.sendEmailConfirmation(user, job.attrs.data.orgTag, this.i18n);
         this.scheduleJobWithTiming('sendEmailConfirmation', {userId: user._id, orgTag: job.attrs.data.orgTag}, job.attrs.data.timingIndex+1);
       }
 
-      this.removeJob(job).then(() => done());
     });
 
 
@@ -44,6 +44,7 @@ var Agenda = (function () {
       let user = await User.findOne({ _id: job.attrs.data.userId });
       let organisation = await Organisation.findOne({_id: job.attrs.data.orgId});
       let oar = user.getOrgAndRecord(job.attrs.data.orgId);
+      await this.removeJob(job);
 
       if(!user.isUnsubscribe) {
         if(!oar.welcomed && user.email.validated) {
@@ -54,7 +55,6 @@ var Agenda = (function () {
         }
       }
 
-      this.removeJob(job).then(() => done());
     });
 
 
@@ -69,13 +69,13 @@ var Agenda = (function () {
       let organisation = await Organisation.findOne({_id : job.attrs.data.orgId});
       let recordId = user.getRecordIdByOrgId(job.attrs.data.orgId);
       let record = await Record.findOne({_id: recordId});
+      await this.removeJob(job);
 
       if(!record.completedAt && !user.isUnsubscribe) {
         await AgendaController.sendEmailPerfectYourProfile(user, organisation, record, this.i18n);
         this.scheduleJobWithTiming('sendEmailPerfectYourProfile', {userId: user._id, orgId: organisation._id}, job.attrs.data.timingIndex+1);
       }
 
-      this.removeJob(job).then(() => done());
     });
 
 
